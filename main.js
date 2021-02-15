3948aa0ab872e8825b9a99d0e62de235487a4d92
Guenole76 <54894682+Guenole76@users.noreply.github.com>
2021-02-15 11:42:18 +0100

    move left
    ajout du deplacement vers la gauche

--- main.js
+++ main.js
@@ -2,12 +2,23 @@ var dodger = {
 	preload: function() {
 		// Charger les fichiers
 		game.load.image('fond', 'assets/fond.jpg');
+		game.load.image('player', 'assets/electru.jpg')
 	},
 	create: function() {
 		
+		game.physics.startSystem(Phaser.Physics.ARCADE);
+
 		game.add.sprite(0, 0, 'fond');
+		this.player =  game.add.sprite(600, 800, 'player');
+
+		game.physics.arcade.enable(this.player);
+
+		this.cursors = game.input.keyboard.createCursorKeys();
 	},
 	update: function() {
+		if(this.cursors.left.isDown){
+			this.player.body.velocity.x = -300;
+		}
 		
 	}
 };
